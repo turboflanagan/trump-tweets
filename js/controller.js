88b8b890d087fc060ff4eb98bc9cc1ec064c38ff
sltiedeman <sltiedeman@gmail.com>
2015-11-19 16:49:10 -0500

    Updated so button works now

--- js/controller.js
+++ js/controller.js
@@ -25,11 +25,22 @@ myApp.controller('myController', function ($scope, $http, $location){
 				$scope.data[i].user.profile_banner_url = "images/trumpbackground.jpg";
 			}
 			console.log($scope.data[i].user.profile_banner_url);
-		}
+		};
 		console.log($scope.data);
 	});
 
 	$scope.getHillaryTweets = function(){
+		var url='http://ec2-52-34-116-224.us-west-2.compute.amazonaws.com/trump-tweets/?hash=%23hillary';
+		$http.get(url).success(function(data2){
+			$scope.data2 = data2.statuses;
+			for(i=0; i<$scope.data2.length;i++){
+				if ($scope.data2[i].user.profile_banner_url == null){
+					$scope.data2[i].user.profile_banner_url = "images/hillarybackground.jpg";
+				}
+				console.log($scope.data2[i].user.profile_banner_url);
+			}
+		$location.path('/hillarytweets');
+	});
 
 	}
 
