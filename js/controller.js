2c1c8064903222a04721c08f636f1a3aceb009e3
sltiedeman <sltiedeman@gmail.com>
2015-11-19 15:08:23 -0500

    Angular Updates

--- js/controller.js
+++ js/controller.js
@@ -1,1 +1,30 @@
-controller.js
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function($routeProvider, $locationProvider){
+	$routeProvider.when('/', {
+		templateUrl: 'trumptweets.html',
+		controller: 'myController'
+	}).
+	when('/hillarytweets',{
+		templateUrl: 'hillarytweets.html',
+		controller: 'myController'
+	}).
+	otherwise({
+		redirectTo: 'trumptweets.html'
+	});
+
+});
+
+myApp.controller('myController', function ($scope, $http, $location){
+
+	var url='http://ec2-52-34-116-224.us-west-2.compute.amazonaws.com/trump-tweets/?hash=%23trump';
+	$http.get(url).success(function(data){
+		$scope.data = data.statuses;
+		console.log($scope.data);
+	});
+
+	$scope.getHillaryTweets = function(){
+
+	}
+
+});
