a66092278b26a283e4258d60b9a38f0ead90f353
sltiedeman <sltiedeman@gmail.com>
2015-11-19 22:09:30 -0500

    Added seconds counter
    Added a counter to display on each tweet page that shows how long since
    the tweet.  Also tweaked a little styling

--- js/controller.js
+++ js/controller.js
@@ -1,3 +1,7 @@
+var tweetSeconds;
+var timeDifference;
+var allData;
+
 var myApp = angular.module('myApp', ['ngRoute']);
 
 myApp.config(function($routeProvider, $locationProvider){
@@ -15,7 +19,7 @@ myApp.config(function($routeProvider, $locationProvider){
 
 });
 
-myApp.controller('myController', function ($scope, $http, $location){
+myApp.controller('myController', function ($scope, $http, $location, $interval){
 
 	var url='http://ec2-52-34-116-224.us-west-2.compute.amazonaws.com/trump-tweets/?hash=%23trump';
 	$http.get(url).success(function(data){
@@ -24,10 +28,26 @@ myApp.controller('myController', function ($scope, $http, $location){
 			if ($scope.data[i].user.profile_banner_url == null){
 				$scope.data[i].user.profile_banner_url = "images/trumpbackground.jpg";
 			}
-			console.log($scope.data[i].user.profile_banner_url);
-		};
+			var time = $scope.data[i].created_at;
+			var tweetTime = new Date(time);
+			var tweetSeconds = tweetTime.getTime()/1000;
+			$scope.data[i].tweetSeconds = tweetSeconds;
+
+		}	
 		console.log($scope.data);
+		$interval(function(){
+			for(i=0; i < $scope.data.length; i++){
+				var currentTime = new Date();
+				var currentTimeInSeconds = currentTime.getTime()/1000;
+				$scope.timeDifference = parseInt(currentTimeInSeconds - $scope.data[i].tweetSeconds);
+				$scope.data[i].timeDifference = $scope.timeDifference;
+			};
+		},1000)	
+
+
 	});
+
+
 
 	$scope.getHillaryTweets = function(){
 		var url='http://ec2-52-34-116-224.us-west-2.compute.amazonaws.com/trump-tweets/?hash=%23hillary';
@@ -37,8 +57,20 @@ myApp.controller('myController', function ($scope, $http, $location){
 				if ($scope.data2[i].user.profile_banner_url == null){
 					$scope.data2[i].user.profile_banner_url = "images/hillarybackground.jpg";
 				}
-				console.log($scope.data2[i].user.profile_banner_url);
+				var time = $scope.data2[i].created_at;
+				var tweetTime = new Date(time);
+				var tweetSeconds = tweetTime.getTime()/1000;
+				$scope.data2[i].tweetSeconds = tweetSeconds;
+
 			}
+			$interval(function(){
+				for(i=0; i < $scope.data2.length; i++){
+					var currentTime = new Date();
+					var currentTimeInSeconds = currentTime.getTime()/1000;
+					$scope.timeDifference = parseInt(currentTimeInSeconds - $scope.data2[i].tweetSeconds);
+					$scope.data2[i].timeDifference = $scope.timeDifference;
+				};
+			},1000)	
 		$location.path('/hillarytweets');
 	});
 
